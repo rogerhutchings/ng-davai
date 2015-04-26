8d197c49b89665981b90fa7b43b73516ed6a67bc
Roger Hutchings <roger@hutchin.gs>
2015-04-26 12:44:47 +0100

    Remove html5 config option from demo module

--- app/modules/demo/demo.routes.js
+++ app/modules/demo/demo.routes.js
@@ -8,8 +8,6 @@ require('./demo.module.js')
  */
 function Routes($stateProvider, $locationProvider, $urlRouterProvider) {
 
-    $locationProvider.html5Mode(true);
-
     $stateProvider
     .state('Home', {
         url: '/',
